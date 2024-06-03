51febcf740db7f40d810654f55634efb9ce8c04b
batu <batu.s@live.com>
2024-06-03 14:58:22 +0200

    commit

--- app.js
+++ app.js
@@ -8,22 +8,30 @@ document.addEventListener("DOMContentLoaded", function () {
       logElement.textContent = "Geolocation is not supported by your browser";
       return;
     }
+    logData += "timestamp,latitude,longitude,altitude\n";
+    logElement.textContent =
+      "Started tracking...\ntimestamp,latitude,longitude,altitude\n";
     intervalId = setInterval(() => {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const { latitude, longitude, altitude } = position.coords;
-          const timestamp = new Date(position.timestamp);
-          const entry = `Time: ${timestamp}, Lat: ${latitude}, Lon: ${longitude}, Alt: ${
-            altitude || "Not available"
-          }\n`;
+          // date format hours:minutes:seconds 24
+          const timestamp = new Date().toTimeString().split(" ")[0];
+          const entry = `${timestamp},${latitude},${longitude},${altitude}\n`;
           logData += entry;
+          const json = JSON.stringify({
+            timestamp,
+            latitude,
+            longitude,
+            altitude,
+          });
           logElement.textContent += entry;
         },
         (error) => {
           logElement.textContent += `Error: ${error.message}\n`;
         }
-      );
-    }, 30000); // 30000 milliseconds = 30 seconds
+      ); // track every  1second
+    }, 1000);
   });
 
   document.getElementById("stopButton").addEventListener("click", () => {
@@ -32,14 +40,16 @@ document.addEventListener("DOMContentLoaded", function () {
       logElement.textContent += "Stopped tracking.\n";
       saveData(logData);
     }
+    logData = "";
   });
 
   function saveData(data) {
-    const blob = new Blob([data], { type: "text/plain" });
+    // csv
+    const blob = new Blob([data], { type: "text/csv" });
     const url = URL.createObjectURL(blob);
     const a = document.createElement("a");
     a.href = url;
-    a.download = "gps_log.txt";
+    a.download = "gps_log.csv";
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
